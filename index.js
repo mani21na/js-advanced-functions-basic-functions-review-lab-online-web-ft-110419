0192bf49ab2f3d22519363526ce7a1fe9b1a70aa
Hanna Lee <mani21na@gmail.com>
2020-03-13 23:30:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
 function wrapAdjective(string = "*") {
   return function (special) {
     let first = "You are"
     return console.log(`${first} ${string} ${special}`)
   }
+}
+
+const Calculator = {
+  add: function(a, b) {
+    return a+b;
+  },
+  subtract: function(a, b) {
+    return a-b;
+  },
+  multiply: function(a, b) {
+    return a*b;
+  },
+  divide: function(a, b) {
+    return a/b;
+  }
+}
+
+function actionApplyer(start, fns) {
+  for (let i = 0; i < fns.length; i++) {
+    start = fns[i](start);
+  }
+  return start;
 }
